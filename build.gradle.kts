51adbc2a944c25967ad508cb91024cc7d204d03d
Steve Hu <stevehu@gmail.com>
2020-08-28 15:00:27 -0400

    upgrade to version 2.0.17 before release

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ java {
 
 allprojects {
     group = "com.networknt"
-    version = "2.0.17-SNAPSHOT"
+    version = "2.0.17"
     repositories {
         mavenLocal() // mavenLocal must be added first.
         jcenter()
