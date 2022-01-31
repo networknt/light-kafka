7141e96950aa51997d124ba0607c8b4a27dfb91d
Steve Hu <stevehu@gmail.com>
2022-01-31 17:02:09 -0500

    Issue131 (#132)
    * fixes #131 add the correlationId to the ProduceRecord to give a chance for the backend producer to pass it
    
    * fixes #131 Also add correlationId to the SerializedKeyAndValue object

--- kafka-producer/src/main/java/com/networknt/kafka/producer/SerializedKeyAndValue.java
+++ kafka-producer/src/main/java/com/networknt/kafka/producer/SerializedKeyAndValue.java
@@ -8,19 +8,21 @@ import java.util.Optional;
 public class SerializedKeyAndValue {
     Optional<Integer> partitionId;
     Optional<String> traceabilityId;
+    Optional<String> correlationId;
     Optional<ByteString> key;
     Optional<ByteString> value;
 
-    public SerializedKeyAndValue(Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<ByteString> key, Optional<ByteString> value) {
+    public SerializedKeyAndValue(Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value) {
         this.partitionId = partitionId;
         this.traceabilityId = traceabilityId;
+        this.correlationId = correlationId;
         this.key = key;
         this.value = value;
     }
 
     public static SerializedKeyAndValue create(
-            Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<ByteString> key, Optional<ByteString> value) {
-        return new SerializedKeyAndValue(partitionId, traceabilityId, key, value);
+            Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value) {
+        return new SerializedKeyAndValue(partitionId, traceabilityId, correlationId, key, value);
     }
 
     public Optional<Integer> getPartitionId() {
@@ -37,6 +39,14 @@ public class SerializedKeyAndValue {
 
     public void setTraceabilityId(Optional<String> traceabilityId) {
         this.traceabilityId = traceabilityId;
+    }
+
+    public Optional<String> getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(Optional<String> correlationId) {
+        this.correlationId = correlationId;
     }
 
     public Optional<ByteString> getKey() {
@@ -60,11 +70,11 @@ public class SerializedKeyAndValue {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SerializedKeyAndValue that = (SerializedKeyAndValue) o;
-        return Objects.equals(partitionId, that.partitionId) && Objects.equals(traceabilityId, that.traceabilityId) && Objects.equals(key, that.key) && Objects.equals(value, that.value);
+        return Objects.equals(partitionId, that.partitionId) && Objects.equals(traceabilityId, that.traceabilityId) && Objects.equals(correlationId, that.correlationId) && Objects.equals(key, that.key) && Objects.equals(value, that.value);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(partitionId, traceabilityId, key, value);
+        return Objects.hash(partitionId, traceabilityId, correlationId, key, value);
     }
 }
