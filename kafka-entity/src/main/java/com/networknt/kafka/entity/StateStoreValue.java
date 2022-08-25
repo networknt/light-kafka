ac8e4e995b5974748236e71eefe717d3bfa389a9
Debashisa Mohanty <debashisam@yahoo.com>
2022-08-25 15:08:03 -0400

    I1488 (#166)
    * updated code for DLQ and audit in streams
    
    * updated code for DLQ and audit in streams
    
    * updated AuditRecordCreation method createAuditRecord to be static
    
    * removed some unused imports
    
    * adding an interface for injecting headers into satestore records in streams
    
    * updated StateStoreValue interface to ignore serialization of default method
    
    Co-authored-by: Debashisa Mohanty <qf90@clarica.com>

---
+++ kafka-entity/src/main/java/com/networknt/kafka/entity/StateStoreValue.java
@@ -0,0 +1,54 @@
+package com.networknt.kafka.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.networknt.utility.Constants;
+import com.networknt.utility.ObjectUtils;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+
+@JsonIgnoreProperties(value = {"headers"})
+public interface StateStoreValue {
+
+    public static final Logger logger = LoggerFactory.getLogger(StateStoreValue.class);
+
+    void setCorrelationId(String correlationId);
+
+    String getCorrelationId();
+
+    void setTraceabilityId(String traceabilityId);
+
+    String getTraceabilityId();
+
+    default void setHeaders(Headers headers) {
+
+        if (!ObjectUtils.isEmpty(headers.lastHeader(Constants.CORRELATION_ID_STRING))) {
+            setCorrelationId(new String(headers.lastHeader(Constants.CORRELATION_ID_STRING).value(), StandardCharsets.UTF_8));
+        } else {
+            logger.error(Constants.CORRELATION_ID_STRING + " is not present in headers");
+        }
+
+        if (!ObjectUtils.isEmpty(headers.lastHeader(Constants.TRACEABILITY_ID_STRING))) {
+            setTraceabilityId(new String(headers.lastHeader(Constants.TRACEABILITY_ID_STRING).value(), StandardCharsets.UTF_8));
+        } else {
+            logger.error(Constants.TRACEABILITY_ID_STRING + " is not present in headers");
+        }
+
+
+    }
+
+    default Headers getHeaders() {
+        Headers headers = new RecordHeaders();
+        headers.add(Constants.CORRELATION_ID_STRING, getCorrelationId().getBytes(StandardCharsets.UTF_8));
+        headers.add(Constants.TRACEABILITY_ID_STRING, getTraceabilityId().getBytes(StandardCharsets.UTF_8));
+
+        return headers;
+    }
+}
+
+
+
