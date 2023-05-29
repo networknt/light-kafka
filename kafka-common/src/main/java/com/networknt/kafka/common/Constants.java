7ffeadc05f71542aaf447b720952a0376924888d
Steve Hu <stevehu@gmail.com>
2023-05-29 16:21:31 -0400

    fixes #180 add a kafka common constant class (#181)

---
+++ kafka-common/src/main/java/com/networknt/kafka/common/Constants.java
@@ -0,0 +1,7 @@
+package com.networknt.kafka.common;
+
+public class Constants {
+    public static final String GENERIC_TRANSFORMER = "genericTransformer";
+    public static final String RAW_SINK = "rawSink";
+
+}
