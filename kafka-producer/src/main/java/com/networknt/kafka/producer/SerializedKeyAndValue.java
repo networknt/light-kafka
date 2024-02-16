12384dd91434bdafbdad32de93f7d001c75fc917
Debashisa Mohanty <debashisa.mohanty@sunlife.com>
2024-02-16 18:30:06 -0500

    updated code to serialize headers and timestamp as part of records

--- kafka-producer/src/main/java/com/networknt/kafka/producer/SerializedKeyAndValue.java
+++ kafka-producer/src/main/java/com/networknt/kafka/producer/SerializedKeyAndValue.java
@@ -2,6 +2,7 @@ package com.networknt.kafka.producer;
 
 import com.google.protobuf.ByteString;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -11,20 +12,38 @@ public class SerializedKeyAndValue {
     Optional<String> correlationId;
     Optional<ByteString> key;
     Optional<ByteString> value;
+    Optional<Map<String, String>> headers;
+    Optional<Long> timestamp;
 
-    public SerializedKeyAndValue(Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value) {
+    public SerializedKeyAndValue(Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value,Optional<Map<String, String>> headers, Optional<Long> timestamp) {
         this.partitionId = partitionId;
         this.traceabilityId = traceabilityId;
         this.correlationId = correlationId;
         this.key = key;
         this.value = value;
+        this.headers= headers;
+        this.timestamp= timestamp;
     }
 
     public static SerializedKeyAndValue create(
-            Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value) {
-        return new SerializedKeyAndValue(partitionId, traceabilityId, correlationId, key, value);
+            Optional<Integer> partitionId, Optional<String> traceabilityId, Optional<String> correlationId, Optional<ByteString> key, Optional<ByteString> value, Optional<Map<String, String>> headers, Optional<Long> timestamp) {
+        return new SerializedKeyAndValue(partitionId, traceabilityId, correlationId, key, value, headers,timestamp);
     }
 
+    public Optional<Long> getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Optional<Long> timestamp) {
+        this.timestamp = timestamp;
+    }
+    public Optional<Map<String, String>> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Optional<Map<String, String>> headers) {
+        this.headers = headers;
+    }
     public Optional<Integer> getPartitionId() {
         return partitionId;
     }
@@ -70,11 +89,11 @@ public class SerializedKeyAndValue {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SerializedKeyAndValue that = (SerializedKeyAndValue) o;
-        return Objects.equals(partitionId, that.partitionId) && Objects.equals(traceabilityId, that.traceabilityId) && Objects.equals(correlationId, that.correlationId) && Objects.equals(key, that.key) && Objects.equals(value, that.value);
+        return Objects.equals(partitionId, that.partitionId) && Objects.equals(traceabilityId, that.traceabilityId) && Objects.equals(correlationId, that.correlationId) && Objects.equals(key, that.key) && Objects.equals(value, that.value) && Objects.equals(headers, that.headers) && Objects.equals(timestamp, that.timestamp);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(partitionId, traceabilityId, correlationId, key, value);
+        return Objects.hash(partitionId, traceabilityId, correlationId, key, value, headers, timestamp);
     }
 }
